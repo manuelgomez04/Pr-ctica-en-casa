2a34298a04351b350fbfe7a702e8dbabc454518a
Manuel Gómez Martín <gomez.maman24@triana.salesianos.edu>
2023-11-26 16:10:35 +0100

    Primer COmmit

--- PruebaGIt/src/hola/Principal.java
+++ PruebaGIt/src/hola/Principal.java
@@ -5,6 +5,7 @@ public class Principal {
 	public static void main(String[] args) {
 		System.out.println("Holaaaa probando, probando");
 		System.out.println("Probando probandos");
+		System.out.println("HGola");
 	}
 
 }
